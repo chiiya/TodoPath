1ca5d269726171c24fee218dffcab817d9f5cf26
vkatheri <v_cia10@hotmail.com>
2016-05-04 18:39:01 +0200

    bitte schon :)

--- app/src/main/java/me/ewitte/todopath/MainActivity.java
+++ app/src/main/java/me/ewitte/todopath/MainActivity.java
@@ -1,15 +1,28 @@
 package me.ewitte.todopath;
 
+import android.content.ContentValues;
+import android.content.DialogInterface;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.ListAdapter;
+import android.widget.SimpleCursorAdapter;
+
+import me.ewitte.todopath.db.TaskContract;
+import me.ewitte.todopath.db.TaskDBHelper;
 
 public class MainActivity extends AppCompatActivity {
+    private TaskDBHelper helper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,10 +35,58 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+                addtask();
+
             }
         });
+    }
+
+    public void addtask () {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Add a task");
+        builder.setMessage("What do you want to do?");
+        final EditText inputField = new EditText(this);
+        builder.setView(inputField);
+        builder.setPositiveButton("Add", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                String task = inputField.getText().toString();
+
+                helper = new TaskDBHelper(MainActivity.this);
+                SQLiteDatabase db = helper.getWritableDatabase();
+                ContentValues values = new ContentValues();
+
+                values.clear();
+                values.put(TaskContract.Columns.TASK, task);
+
+                db.insertWithOnConflict(TaskContract.TABLE, null, values, SQLiteDatabase.CONFLICT_IGNORE);
+                updateUI();
+            }
+        });
+
+        builder.setNegativeButton("Cancel", null);
+
+        builder.create().show();
+    }
+
+    private void updateUI() {
+        helper = new TaskDBHelper(MainActivity.this);
+        SQLiteDatabase sqlDB = helper.getReadableDatabase();
+        Cursor cursor = sqlDB.query(TaskContract.TABLE,
+                new String[]{TaskContract.Columns._ID, TaskContract.Columns.TASK},
+                null, null, null, null, null);
+
+        ListAdapter listAdapter = new SimpleCursorAdapter(
+                this,
+                R.layout.task_view,
+                cursor,
+                new String[]{TaskContract.Columns.TASK},
+                new int[]{R.id.list},
+                0
+        );
+        this.setListAdapter(listAdapter);
     }
 
     @Override
@@ -40,13 +101,21 @@ public class MainActivity extends AppCompatActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+ /*       int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
 
-        return super.onOptionsItemSelected(item);
+        return super.onOptionsItemSelected(item);*/
+        switch (item.getItemId()) {
+            case R.id.action_add_task:
+                Log.d("MainActivity","Add a new task");
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
---
+++ app/src/main/java/me/ewitte/todopath/db/TaskContract.java
@@ -0,0 +1,17 @@
+package me.ewitte.todopath.db;
+
+/**
+ * Created by vicakatherine on 5/4/16.
+ */
+
+import android.provider.BaseColumns;
+
+public class TaskContract {
+    public static final String DB_NAME = "com.example.TodoList.db.tasks";
+    public static final int DB_VERSION = 1;
+    public static final String TABLE = "tasks";
+
+    public class Columns {
+        public static final String TASK = "task";
+        public static final String _ID = BaseColumns._ID;}
+}
